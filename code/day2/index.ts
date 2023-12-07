7ca3a0e11875fad0dda0f3f45b42fab52bad6f28
Julian Hahn <40185075+julianhahn@users.noreply.github.com>
2023-12-07 10:10:23 +0900

    finished day two but didn't wrote a test for it

--- code/day2/index.ts
+++ code/day2/index.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from 'fs'
+
 type set = {
     red: number
     green: number
@@ -49,6 +51,53 @@ export function part_1(input: string): number {
     return total
 }
 
+export function part_2(input: string): number {
+    const lines = splitLines(input)
+    const games: game[] = []
+    for (let line of lines) {
+        const [id, sets] = line.split(': ')
+        games.push({
+            id: parseInt(id.split('Game ')[1]),
+            sets: getSets(line),
+        })
+    }
+    const smallestSets = games.map((game) => {
+        return getSmallestPossibleSet(game)
+    })
+    let total = 0
+    for (let set of smallestSets) {
+        total += calculatePowerOfSet(set)
+    }
+    return total
+}
+
+function calculatePowerOfSet(set: set): number {
+    return set.red * set.green * set.blue
+}
+
+function getSmallestPossibleSet(game: game): set {
+    let maxRed = 0
+    let maxGreen = 0
+    let maxBlue = 0
+    // find the biggest number in each set as a maximum
+    for (let set of game.sets) {
+        if (set.red > maxRed) {
+            maxRed = set.red
+        }
+        if (set.green > maxGreen) {
+            maxGreen = set.green
+        }
+        if (set.blue > maxBlue) {
+            maxBlue = set.blue
+        }
+    }
+    return {
+        red: maxRed,
+        green: maxGreen,
+        blue: maxBlue,
+    }
+}
+
 function getSets(line: string): set[] {
     const resultSet = []
     // split id from sets
@@ -86,3 +135,9 @@ function getSets(line: string): set[] {
 function splitLines(input: string): string[] {
     return input.split(/\r?\n/)
 }
+
+function main() {
+    const input = readFileSync('input.txt', 'utf8')
+    console.log(part_2(input))
+}
+main()
